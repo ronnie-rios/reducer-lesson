93ab6d937175d96569f28b308c68f3a29460b3a4
Ronnie Rios <ronniesmail14@gmail.com>
2023-02-23 15:47:32 -0600

    readme update

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import { useContext, useEffect } from 'react';
+import ApiContext from '../context/ApiContext';
 
 const Comments = () => {
+  const { comments, loading } = useContext(ApiContext);
+  
+
   return (
     <div>Comments</div>
   )
